3dcd4a99b29a370ba51100e7aaa215015a1d8782
April Hung <huotsai2fi@gmail.com>
2025-02-11 16:59:51 +0800

    fix: add token to request header after login

--- week3/src/App.jsx
+++ week3/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
       const response = await request.get(`/api/${VITE_API_PATH}/admin/products`);
       const { products } = response.data;
       setProducts(products);
-    }catch (error) {
+    } catch (error) {
       console.error(error);
     }
   };
@@ -66,6 +66,7 @@ function App() {
       });
       const { token, expired } = response.data;
       document.cookie = `hexToken=${token}; expires=${new Date(expired)};`;
+      request.defaults.headers.common["Authorization"] = token;
       setIsAuth(true);
     } catch (error) {
       const { message } = error.response.data;
@@ -85,92 +86,45 @@ function App() {
   return (
     <>
       {isAuth ? (
-        <div className="container">
-          <div className="row mt-5">
-            <div className="col-md-6">
-              <h2>產品列表</h2>
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>產品名稱</th>
-                    <th>原價</th>
-                    <th>售價</th>
-                    <th>是否啟用</th>
-                    <th>查看細節</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {products ? products.length > 0 ? (
-                    products.map((item) => (
-                      <tr key={item.id}>
-                        <td>{item.title}</td>
-                        <td>{item.origin_price}</td>
-                        <td>{item.price}</td>
-                        <td>{item.is_enabled ? "啟用" : "未啟用"}</td>
-                        <td>
-                          <button
-                            className="btn btn-primary"
-                            onClick={() => setTempProduct(item)}
-                          >
-                            查看細節
-                          </button>
-                        </td>
-                      </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td colSpan="5">尚無產品資料</td>
-                    </tr>
-                  ) : (
-                    <tr>
-                      <td colSpan="5">商品載入中...</td>
-                    </tr>)}
-                </tbody>
-              </table>
+        <div>
+          <div className="container">
+            <div className="text-end mt-4">
+              <button className="btn btn-primary">建立新的產品</button>
             </div>
-            <div className="col-md-6">
-              <h2>單一產品細節</h2>
-              {tempProduct ? (
-                <div className="card mb-3">
-                  <img
-                    src={tempProduct.imageUrl}
-                    className="card-img-top primary-image"
-                    alt="主圖"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      {tempProduct.title}
-                      <span className="badge bg-primary ms-2">
-                        {tempProduct.category}
-                      </span>
-                    </h5>
-                    <p className="card-text">
-                      商品描述：{tempProduct.description}
-                    </p>
-                    <p className="card-text">商品內容：{tempProduct.content}</p>
-                    <div className="d-flex">
-                      <p className="card-text text-secondary">
-                        <del>{tempProduct.origin_price}</del>
-                      </p>
-                      元 / {tempProduct.price} 元
+            <table className="table mt-4">
+              <thead>
+                <tr>
+                  <th width="120">分類</th>
+                  <th>產品名稱</th>
+                  <th width="120">原價</th>
+                  <th width="120">售價</th>
+                  <th width="100">是否啟用</th>
+                  <th width="120">編輯</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td className="text-end"></td>
+                  <td className="text-end"></td>
+                  <td>
+                    <span className="text-success">啟用</span>
+                    <span>未啟用</span>
+                  </td>
+                  <td>
+                    <div className="btn-group">
+                      <button type="button" className="btn btn-outline-primary btn-sm">
+                        編輯
+                      </button>
+                      <button type="button" className="btn btn-outline-danger btn-sm">
+                        刪除
+                      </button>
                     </div>
-                    <h5 className="mt-3">更多圖片：</h5>
-                    <div className="d-flex flex-wrap">
-                      {tempProduct.imagesUrl?.map((url, index) => (
-                        <img
-                          key={index}
-                          src={url}
-                          className="images"
-                          alt="副圖"
-                        />
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              ) : (
-                <p className="text-secondary">請選擇一個商品查看</p>
-              )}
-            </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       ) : (
